f33ecca4afa8772cc2d3d9a4c583d24852a8350b
Kapil Maharjan <kapil@fusemachines.com>
2018-06-26 15:01:48 +0545

    first commit

---
+++ app/src/main/java/app/frantic/eventbus/FirstFragment.java
@@ -0,0 +1,69 @@
+package app.frantic.eventbus;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FirstFragment extends Fragment {
+    TextView textView;
+    Button btn;
+
+    public FirstFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_first, container, false);
+        textView = view.findViewById(R.id.tv_text);
+        btn = view.findViewById(R.id.btn);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EventBus.getDefault().post(new ButtonEvent("Hello from First Fragment!"));
+            }
+        });
+    }
+
+    @Subscribe
+    public void onButtonClick(MainActivity.ButtonFirstEvent event) {
+        textView.setText(event.message);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        EventBus.getDefault().unregister(this);
+    }
+
+}
---
+++ app/src/main/java/app/frantic/eventbus/SecondFragment.java
@@ -0,0 +1,52 @@
+package app.frantic.eventbus;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SecondFragment extends Fragment {
+    TextView textView;
+
+    public SecondFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_second, container, false);
+        textView = view.findViewById(R.id.tv_text);
+        return view;
+    }
+
+    @Subscribe
+    public void onButtonClick(ButtonEvent event) {
+        textView.setText(event.getMessage());
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        EventBus.getDefault().unregister(this);
+    }
+
+}
